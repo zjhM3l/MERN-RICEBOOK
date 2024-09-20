f357f16aea47139757cf640f85c19b3492b4bc4a
zjhM3l <zhangjiahe24@outlook.com>
2024-09-20 00:22:31 -0500

    9/20/2024;recheck

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Feed } from './components/Feed';
 import { Navbar } from './components/Navbar';
 import { Add } from './components/Add';
 import { useState } from 'react';
-import Chat from './components/Chat';
 
 function App() {
 
@@ -20,14 +19,13 @@ function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <Box bgcolor={"background.default"} color={"text.primary"}>
-        {/* <Navbar/>
+        <Navbar/>
         <Stack direction="row" spacing={2} justifyContent="space-between">
           <Sidebar setMode={setMode} mode={mode}/>
           <Feed/>
           <Rightbar/>
         </Stack>
-        <Add/> */}
-        <Chat/>
+        <Add/>
       </Box>
     </ThemeProvider>
   );
