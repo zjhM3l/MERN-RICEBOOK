547d069f9e441a0f2aac5187db8815a5410243f1
zjhM3l <zhangjiahe24@outlook.com>
2024-09-19 19:58:32 -0500

    9/19/2024;nav bar draft

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import { AppBar, Box, styled, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { AppBar, Avatar, Badge, Box, InputBase, Menu, MenuItem, styled, Toolbar, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import FacebookIcon from '@mui/icons-material/Facebook';
+import MailIcon from '@mui/icons-material/Mail';
+import Notifications from '@mui/icons-material/Notifications';
 
 const StyledToolbar = styled(Toolbar)({
   display: 'flex',
@@ -15,18 +17,67 @@ const Search = styled("div")(({ theme }) => ({
 }));
 
 const Icons = styled(Box)(({ theme }) => ({
-  backgroundColor: 'white',
+  display: 'none',
+  gap: '20px',
+  alignItems: 'center',
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex'
+  },  
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({ 
+  display: 'flex', 
+  gap: '10px', 
+  alignItems: 'center',
+  [theme.breakpoints.up('sm')]: {
+    display: 'none'
+  }
 }));
 
 export const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <AppBar position='sticky'>
       <StyledToolbar>
         <Typography variant='h6' sx={{display:{xs:"none", sm:"block"}}}>Rice Book</Typography>
         <FacebookIcon sx={{display:{xs:"block", sm:"none"}}}/>
-        <Search>search</Search>
-        <Icons>Icons</Icons>
+        <Search><InputBase placeholder='search...'/></Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <MailIcon/>
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications/>
+          </Badge>
+          <Avatar 
+          sx={{width:30, height:30}} 
+          src="https://cdn.pixabay.com/photo/2024/08/24/05/02/woman-8993222_1280.jpg"
+          onClick={e => setOpen(true)}
+          />
+        </Icons>
+        <UserBox onClick={e => setOpen(true)}>
+          <Avatar sx={{width:30, height:30}} src="https://cdn.pixabay.com/photo/2024/08/24/05/02/woman-8993222_1280.jpg"/>
+          <Typography variant='span'>Mel</Typography>
+        </UserBox>
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={e => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem >Profile</MenuItem>
+        <MenuItem >My account</MenuItem>
+        <MenuItem >Logout</MenuItem>
+      </Menu>
     </AppBar>
   )
 }
